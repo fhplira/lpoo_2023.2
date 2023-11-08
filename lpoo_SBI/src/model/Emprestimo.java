90451e7ae18e75de732cc0ec63dc172cfa55733a
Davi Monteiro <davimonteirope@gmail.com>
2023-11-08 20:13:07 -0300

    Criação da Classe Emprestimo

---
+++ lpoo_SBI/src/model/Emprestimo.java
@@ -0,0 +1,87 @@
+package model;
+
+public class Emprestimo {
+	
+	private double multa = 0;
+	private int limiteLivro = 0;
+	private int diasAtrasados = 0; 
+	private long cpf;
+	private int idLocal;
+	private long isbn;
+	private String dataEmprestimo;
+	private String dataDevolucao;
+	
+	public Emprestimo(long isbn, int idLocal, long cpf, String dataEmprestimo, String dataDevolucao) {
+		this.isbn = isbn;
+		this.idLocal = idLocal;
+		this.cpf = cpf;
+		this.dataEmprestimo = dataEmprestimo;
+		this.dataDevolucao = dataDevolucao;
+	}
+
+	public double getMulta() {
+		return multa;
+	}
+
+	public void setMulta(double multa) {
+		this.multa = multa;
+	}
+
+	public int getLimiteLivro() {
+		return limiteLivro;
+	}
+
+	public void setLimiteLivro(int limiteLivro) {
+		this.limiteLivro = limiteLivro;
+	}
+
+	public int getDiasAtrasados() {
+		return diasAtrasados;
+	}
+
+	public void setDiasAtrasados(int diasAtrasados) {
+		this.diasAtrasados = diasAtrasados;
+	}
+
+	public long getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(long cpf) {
+		this.cpf = cpf;
+	}
+
+	public int getIdLocal() {
+		return idLocal;
+	}
+
+	public void setIdLocal(int idLocal) {
+		this.idLocal = idLocal;
+	}
+
+	public long getIsbn() {
+		return isbn;
+	}
+
+	public void setIsbn(long isbn) {
+		this.isbn = isbn;
+	}
+
+	public String getDataEmprestimo() {
+		return dataEmprestimo;
+	}
+
+	public void setDataEmprestimo(String dataEmprestimo) {
+		this.dataEmprestimo = dataEmprestimo;
+	}
+
+	public String getDataDevolucao() {
+		return dataDevolucao;
+	}
+
+	public void setDataDevolucao(String dataDevolucao) {
+		this.dataDevolucao = dataDevolucao;
+	}
+
+	
+}
