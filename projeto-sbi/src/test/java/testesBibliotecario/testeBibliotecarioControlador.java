f1f7936748be4fc1a62a65be64105d9221918290
Nayara Maria <dev.nayara@gmail.com>
2023-12-17 17:15:57 -0300

    Adição dos testes do login bibliotecario

--- projeto-sbi/src/test/java/testesBibliotecario/testeBibliotecarioControlador.java
+++ projeto-sbi/src/test/java/testesBibliotecario/testeBibliotecarioControlador.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.fail;
 import static org.mockito.Mockito.when;
 
 import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
@@ -12,7 +13,6 @@ import org.mockito.junit.MockitoJUnitRunner;
 import controladores.BibliotecarioControlador;
 import controladores.ExcecaoControlador;
 import dados.BibliotecarioDados;
-import dados.ExcecaoDados;
 
 @RunWith(MockitoJUnitRunner.class)
 public class testeBibliotecarioControlador {
@@ -31,7 +31,48 @@ public class testeBibliotecarioControlador {
         try {
             controlador.login("exemplo@gmail.com", "senha123");
         } catch (ExcecaoControlador e) {
-            fail();
+            fail("Os dados deveriam ser válidos.");
         }
     }
+    
+    @Test
+    public void falhaLoginEmailVazio() {
+    	Assertions.assertThrows(ExcecaoControlador.class, () -> {controlador.login(" ", "senha123");});		
+    }
+    
+    @Test
+    public void falhaSenhaVazia() {
+    	Assertions.assertThrows(ExcecaoControlador.class, () -> {controlador.login("exemplo@gmail.com", " ");});		
+    }
+    
+    /*@Test
+    public void falhaLoginEmailNulo() {
+    	Assertions.assertThrows(ExcecaoControlador.class, () -> {controlador.login(null, "senha123");});		
+    }
+    
+    @Test
+    public void falhaSenhaNula() {
+    	Assertions.assertThrows(ExcecaoControlador.class, () -> {controlador.login("exemplo@gmail.com", null);});		
+    }*/
+    
+    @Test
+    public void falhaEmailSemCaractere() {
+    	Assertions.assertThrows(ExcecaoControlador.class, () -> {controlador.login("exemplo", "senha123");});		
+    }
+    
+    @Test
+    public void falhaSenhaPequena() {
+    	Assertions.assertThrows(ExcecaoControlador.class, () -> {controlador.login("exemplo", "sen");});		
+    }
+    
+    @Test
+    public void falhaSenhaIncorreta() {
+    	Assertions.assertThrows(ExcecaoControlador.class, () -> {controlador.login("exemplo@gmail.com", "senha12");});		
+    }
+    
+    @Test
+    public void falhaEmailIncorreto() {
+    	Assertions.assertThrows(ExcecaoControlador.class, () -> {controlador.login("exemplo123@gmail.com", "senha12");});		
+    }
+   
 }
